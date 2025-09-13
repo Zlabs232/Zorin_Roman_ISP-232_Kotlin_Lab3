a368f1525dc8c0064e104395cbf380358c4d484b
Роман Зорин <sgena170315@gmail.com>
2025-09-13 16:53:08 +0300

    Задание 1. Угадай число

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,23 @@
-fun main() {
+import kotlin.random.Random
 
+fun main() {
+    //Task 1
+    val sNumber = Random.nextInt(1, 51)
+    var guess: Int
+    var at = 0
+
+    println("Угадайте число от 1 до 50:")
+    while (true) {
+        guess = readln().toInt()
+        at++
+
+        when {
+            guess < sNumber -> println("Больше!")
+            guess > sNumber -> println("Меньше!")
+            else -> {
+                println("Поздравляю! Вы угадали число $sNumber за $at попыток")
+                break
+            }
+        }
+    }
 }
